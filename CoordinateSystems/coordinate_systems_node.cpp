24e78cc7139d3b1e64a214df7aec5c4221414385
zhaohaibin <zhaohaibin@example.com>
2022-09-27 12:46:10 +0800

    添加一个正方形，沿左下角旋转

--- CoordinateSystems/coordinate_systems_node.cpp
+++ CoordinateSystems/coordinate_systems_node.cpp
@@ -4,6 +4,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <GLFW/glfw3.h>
+#include "../base/rectangle_node.h"
 
 coordinate_systems_node::coordinate_systems_node()
 	: node()
@@ -12,7 +13,7 @@ coordinate_systems_node::coordinate_systems_node()
 
 	glm::mat4 model = glm::mat4(1.0);
 	//沿X方向移动-0.5，Y方向移动-0.3，Z方向移动1.0
-	//model = glm::translate(model, glm::vec3(-0.6f, 0.0f, 1.0f));
+	model = glm::translate(model, glm::vec3(-2.0f, 2.0f, -1.0f));
 	//沿Y轴旋转45度
 	//model = glm::rotate(model, 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 
@@ -21,6 +22,7 @@ coordinate_systems_node::coordinate_systems_node()
 	p->set_before_rendering_update_callback(callback);
 	this->add_child(p);
 
+	//添加XYZ坐标
 	vector<point> points;
 	points.push_back(point(0.0, -10.0, 0.0, 1.0, 0.0, 0.0, 1.0));
 	points.push_back(point(0.0, 10.0, 0.0, 1.0, 0.0, 0.0, 1.0));
@@ -29,6 +31,12 @@ coordinate_systems_node::coordinate_systems_node()
 	points.push_back(point(0.0, 0.0, -10.0, 0.0, 0.0, 1.0, 1.0));
 	points.push_back(point(0.0, 0.0, 10.0, 0.0, 0.0, 1.0, 1.0));
 	this->add_child(new line_node(points));
+
+	model = glm::mat4(1.0);
+	node* p_node = new rectangle_node(0.5, 0.5, "../resource/texture/albedo.png", model);
+	p_node->set_id(2);
+	p_node->set_before_rendering_update_callback(callback);
+	this->add_child(p_node);
 }
 
 
@@ -42,13 +50,19 @@ void coordinate_node_before_rendering_update_callback::do_update(node* p)
 	{
 		static glm::mat4 last = p->get_model_matrix();
 		glm::mat4 model = p->get_model_matrix();
-		last = glm::rotate(last, glm::radians(0.01f), glm::vec3(0.0f, 1.0f, 0.0f));
-		model = glm::translate(last, glm::vec3(1.0, 0.0, 0.0));
+		last = glm::rotate(last, glm::radians(0.01f), glm::vec3(1.0f, 1.0f, 0.0f));
+		model = last;
+		model = glm::translate(last, glm::vec3(-1.0, 1.0, 0.0));
 		p->set_model_matrix(model);
 	}
 	else if (p->get_id() == 2)
 	{
-
+		static glm::mat4 last_2 = p->get_model_matrix();
+		glm::mat4 model = p->get_model_matrix();
+		last_2 = glm::rotate(last_2, glm::radians(0.02f), glm::vec3(0.0f, 0.0f, 1.0f));
+		model = last_2;
+		model = glm::translate(model, glm::vec3(0.25, 0.25, 0.0));
+		p->set_model_matrix(model);
 	}
 	else if (p->get_id() == 3)
 	{
