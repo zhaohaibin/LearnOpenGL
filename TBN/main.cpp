13d93cb2a9b74eb23177b507de0096aa2bc6379b
zhaohaibin <993277531@qq.com>
2022-12-13 11:35:29 +0800

    将TBN计算封装成一个函数

--- TBN/main.cpp
+++ TBN/main.cpp
@@ -1,4 +1,30 @@
 #include <glm/glm.hpp>
+
+
+
+void calculate_tbn(const glm::vec3& pos_1, const glm::vec3& pos_2, const glm::vec3& pos_3,
+	const glm::vec2& uv_1, const glm::vec2& uv_2, const glm::vec2& uv_3,
+	glm::vec3& tangent, glm::vec3& bitangent, glm::vec3& normal)
+{
+	glm::vec3 edge_1 = pos_2 - pos_1;
+	glm::vec3 edge_2 = pos_3 - pos_1;
+	glm::vec2 deltaUV_1 = uv_2 - uv_1;
+	glm::vec2 deltaUV_2 = uv_3 - uv_1;
+
+	float f = 1.0f / (deltaUV_1.x * deltaUV_2.y - deltaUV_2.x* deltaUV_1.y);
+
+	tangent.x = f * (deltaUV_2.y * edge_1.x - deltaUV_1.y * edge_2.x);
+	tangent.y = f * (deltaUV_2.y * edge_1.y - deltaUV_1.y * edge_2.y);
+	tangent.z = f * (deltaUV_2.y * edge_1.z - deltaUV_1.y * edge_2.z);
+	tangent = glm::normalize(tangent);
+
+	bitangent.x = f * (-deltaUV_2.x * edge_1.x + deltaUV_1.x * edge_2.x);
+	bitangent.y = f * (-deltaUV_2.x * edge_1.y + deltaUV_1.x * edge_2.y);
+	bitangent.z = f * (-deltaUV_2.x * edge_1.z + deltaUV_1.x * edge_2.z);
+	bitangent = glm::normalize(bitangent);
+
+	normal = glm::cross(tangent, bitangent);
+}
 int main()
 {
 	//∂®“Â∂•µ„
